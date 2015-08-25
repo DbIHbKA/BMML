01eb732521695d782214fffa45e37d07f3e5b46d
Danila <ddbihbka@gmail.com>
2015-08-25 15:06:00 +0300

    Fix bug in formula

--- src/Numeric/BMML/RVMC.hs
+++ src/Numeric/BMML/RVMC.hs
@@ -8,6 +8,7 @@ module Numeric.BMML.RVMC
 import           Numeric.LinearAlgebra.Data (Matrix, Vector)
 import qualified Numeric.LinearAlgebra.HMatrix as H
 
+
 data RVMCModel = RVMCModel
     { mean :: Vector Double
     , covariance :: Matrix Double
@@ -15,17 +16,17 @@ data RVMCModel = RVMCModel
 
 
 initAlpha :: Int -> Vector Double
-initAlpha = H.konst 1.0e-3
+initAlpha = H.konst 1
 
 initY :: Int -> Vector Double
-initY = H.konst 0
+initY = H.konst 0.5
 
 calcB :: Vector Double -> Matrix Double
 calcB y =
     H.diag
         (H.cmap
              (\yi ->
-                   yi * (1 - yi))
+                   (sigmoid yi) * (1 - sigmoid yi))
              y)
 
 calcSigma :: Matrix Double -> Matrix Double -> Vector Double -> Matrix Double
@@ -34,10 +35,14 @@ calcSigma phi b alpha = H.inv (H.tr phi H.<> b H.<> phi + H.diag alpha)
 calcW
     :: Vector Double
     -> Matrix Double
+    -> Matrix Double
+    -> Matrix Double
     -> Vector Double
     -> Vector Double
     -> Vector Double
-calcW alpha phi t y = ((H.inv (H.diag alpha)) H.<> (H.tr phi)) H.#> (t - y)
+calcW mu sigma b phi t y = (sigma H.<> (H.tr phi) H.<> b) H.#> hatt
+  where
+    hatt = phi H.#> mu + (H.inv b) H.#> (t - y)
 
 calcY :: Vector Double -> Matrix Double -> Vector Double
 calcY w phi = H.cmap sigmoid (phi H.#> w)
@@ -52,21 +57,27 @@ fit phi t =
         y = initY n
         b = calcB y
         sigma = calcSigma phi b alpha
-    in go (2 * floor (sqrt (fromIntegral n))) alpha y b (calcW alpha phi t y) sigma
+    in go
+           (2 * floor (sqrt (fromIntegral n)))
+           alpha
+           y
+           b
+           (H.konst 0.1 d)
+           sigma
   where
     go 0 _ _ _ w sigma =
         RVMCModel
         { mean = w
         , covariance = sigma
         }
-    go k alpha y b w sigma = go (k-1) na ny nb nw ns
+    go k alpha y b w sigma = go (k - 1) na ny nb nw ns
       where
         gamma = H.cmap (1 -) (alpha * (H.takeDiag sigma))
         na = gamma / (w * w)
         ny = calcY w phi
         nb = calcB ny
         ns = calcSigma phi nb na
-        nw = calcW na phi t ny
+        nw = calcW w sigma b phi t ny
 
 
 -- | Return probability of phi is from C_1 (t = 1)
